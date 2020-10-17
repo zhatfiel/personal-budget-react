2d1e8d2a4adfd2dadcefdec78dd10e05679633ce
zacharyOH90 <zacharyoh90@gmail.com>
2020-10-16 23:01:32 -0400

    Implemented axios to communicate with backend and build the chart from json

--- src/MyChart/MyChart.js
+++ src/MyChart/MyChart.js
@@ -1,46 +1,91 @@
-import React from 'react';
-import {Pie, Doughnut} from 'react-chartjs-2';
-import axios from 'axios';
+import React from "react";
+import Pie from "react-chartjs-2";
+import axios from "axios";
 
-const state = {
-  labels: [],
-  datasets: [
-    {
+export default class MyChart extends React.Component {
+  //   state = {
+  //     labels: [],
+  //     datasets: [
+  //       {
+  //         backgroundColor: [
+  //           "#ffcd56",
+  //           "#ff6384",
+  //           "#36a2eb",
+  //           "#fd6b19",
+  //           "#ff0000",
+  //           "#32a852",
+  //           "#5132a8",
+  //           "#a83255",
+  //           "#59ff00",
+  //         ],
+  //         data: [],
+  //       },
+  //     ],
+  //   };
+
+  state = {
+    labels: [],
+    datasets: [
+      {
         backgroundColor: [
-            '#ffcd56',
-            '#ff6384',
-            '#36a2eb',
-            '#fd6b19',
-            '#ff0000',
-            '#32a852',
-            '#5132a8',
-            '#a83255',
-            '#59ff00'
         ],
-      data: [65, 59, 80, 81, 56, 27, 88, 11, 98]
-    }
-  ]
-}
+        data: [],
+      },
+    ],
+  };
 
-export default class App extends React.Component {
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/budget")
+      .then((res) => {
+        const labels = [];
+        const datasets = [{
+            backgroundColor: [
+                "#ffcd56",
+                "#ff6384",
+                "#36a2eb",
+                "#fd6b19",
+                "#ff0000",
+                "#32a852",
+                "#5132a8",
+                "#a83255",
+                "#59ff00",
+            ],
+            data:[]
+        }];
+        
+        for (var i = 0; i < res.data.myBudget.length; i++) {
+          //   this.datasets.setState({ data: res.data.myBudget[i].budget });
+          //   this.setState({ labels: res.data.myBudget[i].title });
+          datasets[0].data[i] = res.data.myBudget[i].budget;
+          labels[i] = res.data.myBudget[i].title;
+        }
+        this.setState({ labels });
+        this.setState({ datasets });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div>
         <Pie
-          data={state}
+          data={this.state}
           options={{
-            title:{
-              display:true,
-              text:'Personal Budget',
-              fontSize:20,
-              fontFamily: 'Tahoma, Arial',
-              fontColor: 'black',
-              position:'top'
+            title: {
+              display: true,
+              text: "Personal Budget",
+              fontSize: 20,
+              fontFamily: "Tahoma, Arial",
+              fontColor: "black",
+              position: "top",
             },
-            legend:{
-              display:true,
-              position:'right'
-            }
+            legend: {
+              display: true,
+              position: "right",
+            },
           }}
           width={400}
           height={400}
@@ -48,11 +93,4 @@ export default class App extends React.Component {
       </div>
     );
   }
-
-// componentDidMount() {
-//     axios.get('/public/budgetData.json').then(res => {
-        
-//     })
-// }
-
 }
